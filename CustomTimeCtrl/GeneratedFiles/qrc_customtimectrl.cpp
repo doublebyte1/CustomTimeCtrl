88c3578f0b01004ddbe5399e75365a2109c19b04
doublebyte <jo@.(none)>
2011-12-27 16:44:57 +0100

    - new commit

--- CustomTimeCtrl/GeneratedFiles/qrc_customtimectrl.cpp
+++ CustomTimeCtrl/GeneratedFiles/qrc_customtimectrl.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Resource object code
 **
-** Created: Wed 21. Dec 15:10:21 2011
+** Created: Fri 23. Dec 18:13:07 2011
 **      by: The Resource Compiler for Qt version 4.7.0
 **
 ** WARNING! All changes made in this file will be lost!
--- CustomTimeCtrl/GeneratedFiles/ui_datetime.h
+++ CustomTimeCtrl/GeneratedFiles/ui_datetime.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'datetime.ui'
 **
-** Created: Wed 21. Dec 15:10:20 2011
+** Created: Fri 23. Dec 18:13:07 2011
 **      by: Qt User Interface Compiler version 4.7.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
